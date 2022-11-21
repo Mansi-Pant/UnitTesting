83f45fce84497efff565f43edcfb5f4f55c53f3c
ASUS <ASUS@10.12.59.96>
2022-11-21 14:55:06 +0530

    add numbers and strings

--- UnitTesting/src/jUnitTest/jUnitFunctions.java
+++ UnitTesting/src/jUnitTest/jUnitFunctions.java
@@ -2,4 +2,23 @@ package jUnitTest;
 
 public class jUnitFunctions {
 
+	void AddSum(int num1, int num2)
+	{
+		int sum;
+		sum = num1 + num2;
+		System.out.println("The sum of numbers is: "+sum);
+	}
+	void TestStringConcatenation1(String s1, String s2)
+	{
+		String s = s1 + s2;
+		System.out.println(s);
+	}
+	public static void main(String args[]) {
+		int num1 = 225, num2 = 115; 
+		String s1= "Mansi";
+		String s2="Pant";
+		jUnitFunctions ob = new jUnitFunctions();
+		ob.AddSum(num1, num2);
+		ob.TestStringConcatenation1(s1, s2);
+	}
 }
--- UnitTesting/src/module-info.java
+++ UnitTesting/src/module-info.java
@@ -1,2 +1,3 @@
 module UnitTesting {
+	requires junit;
 }
